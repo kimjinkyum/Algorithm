d25c82811af3a6f125ac843e81595e0b6661bfa3
kim jingyeom <44569994+kimjinkyum@users.noreply.github.com>
2022-03-15 01:56:01 +0900

    [Bronze I] Title: 이항 계수 1, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -2,12 +2,19 @@ import sys
 
 N, K = sys.stdin.readline().rstrip().split(" ")
 
-def fact(N):
-    result = 1
-    for i in range(2, N+1):
-        result *= i
-    return result
+def fact_dp(N, K):
 
+    cache = [[0 for _ in range(K+1)] for _ in range(N+1)]
 
-result = fact(int(N)) / (fact(int(K)) * fact(int(N) - int(K)))
-print(int(result))
+    for i in range(0, N+1):
+        cache[i][0] = 1
+    for i in range(0, K+1):
+        cache[i][i] = 1
+    for i in range(1, N+1):
+        for j in range(1, K+1):
+            cache[i][j] = cache[i-1][j] + cache[i-1][j-1]
+            
+    
+    return cache[N][K]
+
+print(fact_dp(int(N),  int(K)))
