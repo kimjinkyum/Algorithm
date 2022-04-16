bf082aee41cd16afd49bcd55b76d6bbab11be446
kimjinkyum <kimo1113@daum.net>
2022-04-17 02:50:16 +0900

    [MODIFY]_Greedy_03

--- 이것이 취업을 위한 코딩 테스트다/11_그리디/3.py
+++ 이것이 취업을 위한 코딩 테스트다/11_그리디/3.py
@@ -2,41 +2,58 @@
 import sys
 
 
+def mine(data):
+    count0 = 0
+    count1 = 0
+    change0 = False
+    change1 = False
+
+    for i in range(len(data)):
+        if data[i] != "0":
+            change0 = True
+            i += 1
+
+        else:
+            if change0:
+                count0 += 1
+                change0 = False
+            i += 1
+
+    for i in range(len(data)):
+        if data[i] != "1":
+            change1 = True
+            i += 1
+
+        else:
+            if change1:
+                count1 += 1
+                change1 = False
+            i += 1
+
+    if change0:
+        count0 += 1
+
+    if change1:
+        count1 += 1
+
+    return min(count0, count1)
+
+
 data = sys.stdin.readline().rstrip()
 
 count0 = 0
 count1 = 0
-change0 = False
-change1 = False
 
-for i in range(len(data)):
-    if data[i] != "0":
-        change0 = True
-        i += 1
-
-    else:
-        if change0:
-            count0 += 1
-            change0 = False
-        i += 1
-
-for i in range(len(data)):
-    if data[i] != "1":
-        change1 = True
-        i += 1
-
-    else:
-        if change1:
-            count1 += 1
-            change1 = False
-        i += 1
-
-if change0:
+if data[0] == "1":
     count0 += 1
-
-if change1:
+else:
     count1 += 1
 
+for i in range(len(data) - 1):
+    if data[i] != data[i + 1]:
+        if data[i + 1] == "1":
+            count0 += 1
+        else:
+            count1 += 1
 
 print(min(count0, count1))
-
