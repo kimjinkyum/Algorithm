e4c9418375bddd6bc3472d50052371b2e414dc2b
kimjinkyum <kimo1113@daum.net>
2022-04-17 02:35:59 +0900

    [MODIFY]_Greedy_05

--- 이것이 취업을 위한 코딩 테스트다/11_그리디/5.py
+++ 이것이 취업을 위한 코딩 테스트다/11_그리디/5.py
@@ -4,14 +4,28 @@ import sys
 from itertools import combinations
 
 n, m = map(int, (input().split(" ")))
-
 data = list(map(int, sys.stdin.readline().rstrip().split(" ")))
 
+arr = [0] * 11
+
+for x in data:
+    arr[x] += 1
+
+result = 0
+
+for i in range(1, m+1):
+    n -= arr[i]
+    result += arr[i] * n
+
+print(result)
+
+
+# 내 풀이
 combi = list(combinations([i for i in range(n)], 2))
 
 result = len(combi)
 for x, y in combi:
     if data[x] == data[y]:
-        result -=1
+        result -= 1
 
 print(result)
