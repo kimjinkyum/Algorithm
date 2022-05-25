6557edefda373ce3b1b81f96f5c3d6f5ddb16985
kimjinkyum <kimo1113@daum.net>
2022-05-25 09:43:49 +0900

    [Modify]_DFS_BFS_19

--- 이것이 취업을 위한 코딩 테스트다/13_DFS_BFS 문제/19.py
+++ 이것이 취업을 위한 코딩 테스트다/13_DFS_BFS 문제/19.py
@@ -4,18 +4,31 @@ import sys
 from itertools import product, permutations
 
 
-def cal(s, data):
-    result = data[0]
-    for i in range(1, len(data)):
-        if s[i - 1] == "+":
-            result += data[i]
-        elif s[i - 1] == "-":
-            result -= data[i]
-        elif s[i - 1] == "*":
-            result *= data[i]
-        else:
-            result = int(result / data[i])
-    return result
+def dfs(i, now):
+    global min_value, max_value, add, sub, mul, div
+
+    if i == n:
+        min_value = min(min_value, now)
+        max_value = max(max_value, now)
+
+    else:
+        if add > 0:
+            add -= 1
+            dfs(i + 1, now + data[i])
+            add += 1
+        if sub > 0:
+            sub -= 1
+            dfs(i + 1, now - data[i])
+            sub += 1
+
+        if mul > 0:
+            mul -= 1
+            dfs(i + 1, now * data[i])
+            mul += 1
+        if div > 0:
+            div -= 1
+            dfs(i + 1, int(now / data[i]))
+            div += 1
 
 
 n = int(input())
@@ -24,22 +37,10 @@ data = list(map(int, sys.stdin.readline().rstrip().split()))
 
 add, sub, mul, div = map(int, sys.stdin.readline().rstrip().split())
 
-sign = []
-
-sign.extend(['+'] * add)
-sign.extend(['-'] * sub)
-sign.extend(['*'] * mul)
-sign.extend(['/'] * div)
-
-candi = (list(permutations(sign, n - 1)))
-
 min_value = 1e9
 max_value = -1e9
 
-for c in candi:
-    result = cal(c, data)
-    min_value = min(min_value, result)
-    max_value = max(max_value, result)
+dfs(1, data[0])
 
 print(max_value)
 print(min_value)
