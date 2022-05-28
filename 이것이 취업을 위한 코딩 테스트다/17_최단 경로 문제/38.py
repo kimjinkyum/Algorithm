5b21476d028eab513ba1200d92fb06beac08551e
kimjinkyum <kimo1113@daum.net>
2022-05-28 19:58:28 +0900

    Path_38

---
+++ 이것이 취업을 위한 코딩 테스트다/17_최단 경로 문제/38.py
@@ -0,0 +1,33 @@
+# 정확한 순위
+import sys
+
+INF = int(1e9)
+n, m = map(int, input().split())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    graph[a][b] = 1
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i == j:
+            graph[i][j] = 0
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+result = 0
+
+for i in range(1, n + 1):
+    count = 0
+    for j in range(1, n + 1):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            count += 1
+    if count == n:
+        result += 1
+
+print(result)
