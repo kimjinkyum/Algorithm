d5ba329b56dfce24027a4b9b2903785f6672f14a
kimjinkyum <kimo1113@daum.net>
2022-07-08 13:19:46 +0900

    Graph_41

---
+++ 이것이 취업을 위한 코딩 테스트다/18_그래프 이론 문제/41.py
@@ -0,0 +1,45 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+
+parent = [0] * (n+1)
+
+for i in range(n+1):
+    parent[i] = i
+
+print(parent)
+for i in range(n):
+    data = list(map(int, input().split()))
+
+    for j in range(n):
+        if data[j] == 1:
+            union_parent(parent, i, j)
+    print(parent)
+
+plan = list(map(int, input().split()))
+
+result = True
+
+for i in range(m - 1):
+    if find_parent(parent, plan[i]) != find_parent(parent, plan[i+1]):
+        rsult = False
+
+if result:
+    print("YES")
+else:
+    print("NO")
+
